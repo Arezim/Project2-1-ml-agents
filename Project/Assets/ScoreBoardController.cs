d666c80c2cf9c3b1c5b6abdc6d99c7f886a3b3b8
Ty Baranski <tymek@baranscy.info>
2024-09-25 12:55:56 +0200

    Finished the scoreboard 3d representation
    For now it doesn't bind to the values

--- Project/Assets/ScoreBoardController.cs
+++ Project/Assets/ScoreBoardController.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoardController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text Team1Text;
+    public TMP_Text Team2Text;
+
+    public void Init()
     {
-        
+        // Bind OnScoreChange(...) to the script
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnScoreChange(int? score1 = null, int? score2 = null)
     {
-        
+        if (score1 is int value1)
+        {
+            Team1Text.SetText(FormatScore(value1));
+        }
+        if (score2 is int value2)
+        {
+            Team1Text.SetText(FormatScore(value2));
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Unbind OnScoreChange(...) from the script
+    }
+
+    private string FormatScore(int value)
+    {
+        value = Mathf.Min(99, value);
+        string text = value.ToString();
+        return text.PadLeft(Mathf.Max(0, 2 - text.Length), '0');
     }
 }
