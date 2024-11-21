74b490a3ff2d9c011e4e0982e07ca1ffdeeace30
alepskn <alexandraplishkin@gmail.com>
2024-11-21 18:19:11 +0100

    removed redundant order

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoundSystem/SoundSensorComponent.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoundSystem/SoundSensorComponent.cs
@@ -66,9 +66,6 @@ public class SoundSensorComponent : SensorComponent, ISoundListener, ISensor
     public void OnHearSound(Sound sound)
     {
         sounds.Add(sound);
-        sounds = sounds
-                .OrderByDescending(s => s.Radius / Vector3.Distance(transform.position, s.Origin))
-                .ToList();
     }
 
     public SoccerEnvController GetPlayingField()
