2f38b635c1e5f81c866923aaf0072e0ea0c34d2a
Ty Baranski <tymek@baranscy.info>
2025-01-08 12:11:47 +0100

    After merge fix

--- Project/Assets/EnvironmentVars.cs
+++ Project/Assets/EnvironmentVars.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class EnvironmentVars : MonoBehaviour
 {
@@ -13,7 +14,16 @@ public class EnvironmentVars : MonoBehaviour
     {
         m_statsRecorder = Academy.Instance.StatsRecorder;
         m_nextUpdate = 0;
+
+        Debug.Log("Starting logging into file \"soccerLog\"");
+        Profiler.logFile = "soccerLog";
+        Profiler.enableBinaryLog = true;
+        Profiler.enabled = true;
+        Profiler.maxUsedMemory = 256 * 1024 * 1024;
+        Profiler.BeginSample("Soccer");
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -25,5 +35,13 @@ public class EnvironmentVars : MonoBehaviour
             // Very dirty and "temporary"
             m_nextUpdate += 10000;
         }
+    }
+
+    void onDestroy()
+    {
+        Profiler.EndSample();
+        Profiler.enabled = false;
+        Profiler.logFile = "";
+        Debug.Log("Ending logging!");
     }
 }
